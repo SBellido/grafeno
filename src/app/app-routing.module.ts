a1868eed7d9f26008993a95ae863088ebb4ac8c7
Stormtech Solutions <sebastianbellidodg@gmail.com>
2021-12-20 19:05:51 -0300

    Installation of environment and first modules

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorFourZeroFourComponent } from './errors/error-four-zero-four/error-four-zero-four.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'grafeno',
+        loadChildren: () => import('./grafeno/grafeno.module')
+          .then(m => m.GrafenoModule)
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'admin/login' 
+      },
+      {
+        path: '**',
+        component: ErrorFourZeroFourComponent
+      }
+    ]
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/grafeno/grafeno.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GrafenoRoutingModule } from './grafeno-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GrafenoRoutingModule
+  ]
+})
+export class GrafenoModule { }
